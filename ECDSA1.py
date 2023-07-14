7a89fd8d74ac38becc771edab87eaed82b0ae005
Сіплий <maksdrap228@gmail.com>
2023-07-14 21:38:12 +0300

    Реалізація ECDSA з обгортками

--- ECDSA1.py
+++ ECDSA1.py
@@ -1,25 +1,88 @@
-from lab1 import generate_keypair, hash_message, sign_message, verify_signature
-from cryptography.hazmat.primitives import serialization
+from lab1 import MyECDSA
+
+class ECDSA:
+    @staticmethod
+    def sign_message(private_key, message):
+        # Підписує повідомлення за допомогою приватного ключа.
+        hashed_message = ECDSA.hash_message(message)
+        signature = MyECDSA.sign_message(private_key, hashed_message)
+        return signature
+
+    @staticmethod
+    def hash_message(message):
+        # Обчислює хеш повідомлення.
+        return MyECDSA.hash_message(message)
+
+    @staticmethod
+    def generate_keypair(curve='P-256'):
+        # Генерує пару ключів
+        return MyECDSA.generate_keypair(curve)
+
+    @staticmethod
+    def verify_signature(public_key, message, signature):
+        # Перевіряє підпис повідомлення.
+        hashed_message = ECDSA.hash_message(message)
+        return MyECDSA.verify_signature(public_key, hashed_message, signature)
+
+    @staticmethod
+    def serialize_private_key(private_key):
+        # Серіалізує приватний ключ.
+        return MyECDSA.serialize_private_key(private_key)
+
+    @staticmethod
+    def deserialize_private_key(curve, serialized_private_key):
+        # Десеріалізує приватний ключ.
+        return MyECDSA.deserialize_private_key(curve, serialized_private_key)
+
+    @staticmethod
+    def serialize_public_key(public_key):
+        # Серіалізує публічний ключ.
+        return MyECDSA.serialize_public_key(public_key)
+
+    @staticmethod
+    def deserialize_public_key(curve, serialized_public_key):
+        # Десеріалізує публічний ключ.
+        return MyECDSA.deserialize_public_key(curve, serialized_public_key)
+
+    @staticmethod
+    def serialize_signature(signature):
+        # Серіалізує підпис.
+        return MyECDSA.serialize_signature(signature)
+
+    @staticmethod
+    def deserialize_signature(serialized_signature):
+        # Десеріалізує підпис.
+        return MyECDSA.deserialize_signature(serialized_signature)
+
+def sign_and_verify_message(message):
+    # Еліптична крива (параметри)
+    curve = 'P-256'
+
+    # Згенерувати пару ключів
+    private_key, public_key = ECDSA.generate_keypair(curve)
+
+    # Перетворити повідомлення в байтовий рядок
+    message_bytes = message.encode()
+
+    # Підписати повідомлення
+    signature = ECDSA.sign_message(private_key, message_bytes)
+
+    # Перевірити підпис
+    is_valid = ECDSA.verify_signature(public_key, message_bytes, signature)
+
+    return signature, is_valid
 
 
-# Генерація пари ключів
-private_key, public_key = generate_keypair(curve='P-256')
+# Ввід повідомлення
+message = input("Введіть повідомлення для підписування: ")
 
-# Повідомлення для підпису
-message = "Hello, world!"
+# Підписати повідомлення та перевірити підпис
+signature, is_valid = sign_and_verify_message(message)
 
-# Гешування повідомлення
-hashed_message = hash_message(message)
+# Вивести отриманий підпис
+print("Signature:", ECDSA.serialize_signature(signature))
 
-# Підписання повідомлення
-signature = sign_message(private_key, message)
-
-# Перевірка цифрового підпису
-valid = verify_signature(public_key, message, signature)
-
-# Виведення результатів
-print("Private Key:", private_key.private_numbers().private_value)
-print("Public Key:", public_key.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo))
-print("Hashed Message:", hashed_message)
-print("Signature:", signature)
-print("Valid Signature:", valid)
+if is_valid:
+    print("Підпис перевірено: Повідомлення є валідним.")
+else:
+    print("Підпис не перевірено: Повідомлення є недійсним.")
--- lab1.py
+++ lab1.py
@@ -2,52 +2,54 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec, utils
 from Crypto.PublicKey import ECC
 
-def sign_message(private_key, message):
-    # Підписання повідомлення
-    hashed_message = hash_message(message)
-    signature = private_key.sign(hashed_message, ec.ECDSA(utils.Prehashed(hashes.SHA256())))
-    return signature
+class MyECDSA:
+    @staticmethod
+    def sign_message(private_key, message):
+        hashed_message = MyECDSA.hash_message(message)
+        signature = private_key.sign(hashed_message, ec.ECDSA(utils.Prehashed(hashes.SHA256())))
+        return signature
 
-def hash_message(message):
-    # Гешування повідомлення
-    digest = hashes.Hash(hashes.SHA256())
-    digest.update(message.encode())
-    return digest.finalize()
+    @staticmethod
+    def hash_message(message):
+        digest = hashes.Hash(hashes.SHA256())
+        digest.update(message)
+        return digest.finalize()
 
-def generate_keypair(curve='P-256'):
-    # Генерація пари ключів
-    private_key = ec.generate_private_key(ec.SECP256R1())
-    public_key = private_key.public_key()
-    return private_key, public_key
-def verify_signature(public_key, message, signature):
-    # Перевірка цифрового підпису
-    hashed_message = hash_message(message)
-    try:
-        public_key.verify(signature, hashed_message, ec.ECDSA(utils.Prehashed(hashes.SHA256())))
-        return True
-    except:
-        return False
+    @staticmethod
+    def generate_keypair(curve='P-256'):
+        private_key = ec.generate_private_key(ec.SECP256R1())
+        public_key = private_key.public_key()
+        return private_key, public_key
 
-def serialize_private_key(private_key):
-    # Серіалізація особистого ключа
-    return private_key.to_string().hex()
+    @staticmethod
+    def verify_signature(public_key, message, signature):
+        hashed_message = MyECDSA.hash_message(message)
+        try:
+            public_key.verify(signature, hashed_message, ec.ECDSA(utils.Prehashed(hashes.SHA256())))
+            return True
+        except:
+            return False
 
-def deserialize_private_key(curve, serialized_private_key):
-    # Десеріалізація особистого ключа
-    return ECC.construct(curve=curve, d=int(serialized_private_key, 16))
+    @staticmethod
+    def serialize_private_key(private_key):
+        return private_key.to_string().hex()
 
-def serialize_public_key(public_key):
-    # Серіалізація відкритого ключа
-    return public_key.to_string().hex()
+    @staticmethod
+    def deserialize_private_key(curve, serialized_private_key):
+        return ECC.construct(curve=curve, d=int(serialized_private_key, 16))
 
-def deserialize_public_key(curve, serialized_public_key):
-    # Десеріалізація відкритого ключа
-    return ECC.import_key(serialized_public_key)
+    @staticmethod
+    def serialize_public_key(public_key):
+        return public_key.to_string().hex()
 
-def serialize_signature(signature):
-    # Серіалізація цифрового підпису
-    return signature
+    @staticmethod
+    def deserialize_public_key(curve, serialized_public_key):
+        return ECC.import_key(serialized_public_key)
 
-def deserialize_signature(serialized_signature):
-    # Десеріалізація цифрового підпису
-    return serialized_signature
+    @staticmethod
+    def serialize_signature(signature):
+        return signature
+
+    @staticmethod
+    def deserialize_signature(serialized_signature):
+        return serialized_signature
