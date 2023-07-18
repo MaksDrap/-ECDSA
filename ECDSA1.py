28142c74c2960ec98360e637d33789a635ff7951
Сіплий <maksdrap228@gmail.com>
2023-07-18 13:28:43 +0300

    Реалізація ECDSA з обгортками

--- ECDSA1.py
+++ ECDSA1.py
@@ -2,27 +2,56 @@ from lab1 import MyECDSA
 
 class ECDSA:
     @staticmethod
-    def sign_message(private_key, message):
-        # Підписує повідомлення за допомогою приватного ключа.
-        hashed_message = ECDSA.hash_message(message)
+    def sign_message(message):
+        # Крок 1: Згенерувати випадковий приватний ключ d
+        private_key, _ = MyECDSA.generate_keypair()
+
+        # Крок 2: Обчислити публічний ключ Q як Q = d * G
+        public_key = private_key.public_key()
+
+        # Крок 3: Обчислити хеш-значення повідомлення h(m) як h(m) = SHA256(m)
+        hashed_message = MyECDSA.hash_message(message.encode())  # Перетворити повідомлення в байтовий рядок
+
+        # Крок 4: Використовувати приватний ключ d для генерації підпису (r, s) як (r, s) = ECDSA_sign(h(m), d)
         signature = MyECDSA.sign_message(private_key, hashed_message)
+
+        # Крок 5: Повернути підпис (r, s) як кортеж
         return signature
 
     @staticmethod
     def hash_message(message):
         # Обчислює хеш повідомлення.
-        return MyECDSA.hash_message(message)
-
-    @staticmethod
-    def generate_keypair(curve='P-256'):
-        # Генерує пару ключів
-        return MyECDSA.generate_keypair(curve)
+        import hashlib
+        hash_object = hashlib.sha256(message)
+        return hash_object.digest()
 
     @staticmethod
     def verify_signature(public_key, message, signature):
         # Перевіряє підпис повідомлення.
         hashed_message = ECDSA.hash_message(message)
-        return MyECDSA.verify_signature(public_key, hashed_message, signature)
+        r, s = signature
+
+        if r < 1 or r >= public_key.curve.order or s < 1 or s >= public_key.curve.order:
+            return False
+
+        # Обчислити `w`.
+        w = MyECDSA.mod_inverse(s, public_key.curve.order)
+
+        # Обчислити `u1`.
+        u1 = (hashed_message * w) % public_key.curve.order
+
+        # Обчислити `u2`.
+        u2 = (r * w) % public_key.curve.order
+
+        # Обчислити точку `Q`.
+        Q = MyECDSA.point_addition(MyECDSA.scalar_mult(u1, public_key.curve.generator),
+                           MyECDSA.scalar_mult(u2, public_key.public_numbers().y))
+
+        # Обчислити `v`.
+        v = int.from_bytes(Q, 'big') % public_key.curve.order
+
+        # Повернути `v` дорівнює `r`.
+        return v == r
 
     @staticmethod
     def serialize_private_key(private_key):
@@ -55,34 +84,31 @@ class ECDSA:
         return MyECDSA.deserialize_signature(serialized_signature)
 
 def sign_and_verify_message(message):
-    # Еліптична крива (параметри)
+    # Параметри еліптичної кривої
     curve = 'P-256'
 
-    # Згенерувати пару ключів
-    private_key, public_key = ECDSA.generate_keypair(curve)
+    # Створіть пару ключів
+    private_key, public_key = MyECDSA.generate_keypair(curve)
 
-    # Перетворити повідомлення в байтовий рядок
-    message_bytes = message.encode()
+    # Підпис повідомлення
+    signature = ECDSA.sign_message(message)
 
-    # Підписати повідомлення
-    signature = ECDSA.sign_message(private_key, message_bytes)
-
-    # Перевірити підпис
-    is_valid = ECDSA.verify_signature(public_key, message_bytes, signature)
+    # Перевірка повідомлення
+    is_valid = ECDSA.verify_signature(public_key, message.encode(), signature)  # Convert message to bytes
 
     return signature, is_valid
 
 
-# Ввід повідомлення
-message = input("Введіть повідомлення для підписування: ")
+# Ввести повідомлення
+message = input("Введіть повідомлення для підпису: ")
 
-# Підписати повідомлення та перевірити підпис
+# Підпис і перевірка повідомлення
 signature, is_valid = sign_and_verify_message(message)
 
 # Вивести отриманий підпис
-print("Signature:", ECDSA.serialize_signature(signature))
+print("Підпис:", signature)
 
 if is_valid:
-    print("Підпис перевірено: Повідомлення є валідним.")
+    print("Підпис перевірено: повідомлення дійсне.")
 else:
-    print("Підпис не перевірено: Повідомлення є недійсним.")
+    print("Підпис перевірено: повідомлення не дійсне")
--- lab1.py
+++ lab1.py
@@ -1,7 +1,8 @@
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec, utils
 from Crypto.PublicKey import ECC
-
+import random
+import math
 class MyECDSA:
     @staticmethod
     def sign_message(private_key, message):
@@ -53,3 +54,59 @@ class MyECDSA:
     @staticmethod
     def deserialize_signature(serialized_signature):
         return serialized_signature
+
+    @staticmethod
+    def random_scalar(n):
+        while True:
+            k = random.randint(1, n - 1)
+            if math.gcd(k, n) == 1:
+                return k
+
+    @staticmethod
+    def scalar_mult(k, point):
+        # Convert the raw byte string to an elliptic curve point
+        P = ec.EllipticCurvePublicNumbers.from_encoded_point(point, ec.SECP256R1()).public_key()
+
+        # Get the curve parameters
+        curve = ec.SECP256R1()
+        p = curve.p
+
+        # Perform scalar multiplication k * P
+        Q = P.public_numbers().curve.generator * k
+
+        # Encode the resulting point Q as a raw byte string and return
+        Q_encoded = Q.public_numbers().encode_point()
+        return Q_encoded
+
+    @staticmethod
+    def point_addition(point1, point2):
+        # Convert the raw byte strings to elliptic curve points
+        P = ec.EllipticCurvePublicNumbers.from_encoded_point(point1, ec.SECP256R1()).public_key()
+        Q = ec.EllipticCurvePublicNumbers.from_encoded_point(point2, ec.SECP256R1()).public_key()
+
+        # Get the curve parameters
+        curve = ec.SECP256R1()
+        a = curve.a
+        p = curve.p
+
+        if P == Q:
+            # If P and Q are equal, perform a doubling operation
+            s = (3 * P.x * P.x + a) * pow(2 * P.y, -1, p)
+        else:
+            # If P and Q are distinct, calculate the slope s as (Q_y - P_y) / (Q_x - P_x)
+            s = (Q.y - P.y) * pow(Q.x - P.x, -1, p)
+
+        # Calculate the new point R
+        R_x = (s * s - P.x - Q.x) % p
+        R_y = (s * (P.x - R_x) - P.y) % p
+
+        # Encode the new point as a raw byte string and return
+        R_encoded = ec.EllipticCurvePublicNumbers(R_x, R_y, curve).encode_point()
+        return R_encoded
+
+    @staticmethod
+    def mod_inverse(a, m):
+        """Calculate the modular inverse of a modulo m."""
+        if math.gcd(a, m) != 1:
+            raise ValueError("The inverse does not exist.")
+        return pow(a, -1, m)
